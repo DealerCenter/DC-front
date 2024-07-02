81555bde55f866ebb6ab2742368372de88a2d2d8
zuka.j <zura.j@kumba.pet>
2024-07-03 00:52:50 +0400

    Feat: added file structure

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import AppButton from '@/common/components/appButton/AppButton';
+import React from 'react';
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <div>
+      Auth
+      <AppButton />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/common/components/appButton/AppButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const AppButton = (props: Props) => {
+  return <button>AppButton</button>;
+};
+
+export default AppButton;
