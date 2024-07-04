45f32c683720b8778bc83f7ba863280e456a0c6f
luka-create <wilosani.luka@gmail.com>
2024-07-04 21:09:00 +0400

    worked on login form

---
+++ src/common/components/checkbox/Checkbox.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {};
+
+const Checkbox = (props: Props) => {
+  return <StyledInput type="checkbox" />;
+};
+
+export default Checkbox;
+
+const StyledInput = styled.input`
+  width: 24px;
+  height: 24px;
+  border: 1.5px solid gray;
+  border-radius: 6px;
+  color: red;
+  background-color: red;
+`;
