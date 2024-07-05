86a095a3e28a9d47e29931a5e63f7b72d8e1de6a
luka-create <wilosani.luka@gmail.com>
2024-07-05 13:42:58 +0400

    added burger menu

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import AppButton from "@/common/components/appButton/AppButton";
-import AppButtonEmpty from "@/common/components/appButton/AppButtonEmpty";
-import Checkbox from "@/common/components/checkbox/Checkbox";
 import LoginForm from "./components/LoginForm";
 
 type Props = {};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/common/components/header/Header";
 import { useMediaQuery } from "react-responsive";
+import BurgerHeader from "@/common/components/header/BurgerHeader";
 
 export default function Home() {
   const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
 
-  return <main>{!isMobile && <Header />}</main>;
+  return <main>{isMobile ? <BurgerHeader /> : <Header />}</main>;
 }
