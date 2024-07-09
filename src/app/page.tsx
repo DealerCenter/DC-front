8cafa8f4e9953303f055f3f822789296aa59d98c
luka-create <wilosani.luka@gmail.com>
2024-07-09 11:38:25 +0400

    added small header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,15 @@ import styles from "./page.module.css";
 import Header from "@/common/components/header/Header";
 import { useMediaQuery } from "react-responsive";
 import BurgerHeader from "@/common/components/header/BurgerHeader";
+import LoginForm from "./auth/components/LoginForm";
 
 export default function Home() {
   const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
 
-  return <main>{isMobile ? <BurgerHeader /> : <Header />}</main>;
+  return (
+    <main>
+      {isMobile ? <BurgerHeader /> : <Header />}
+      <LoginForm />
+    </main>
+  );
 }
