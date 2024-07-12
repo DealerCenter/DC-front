e5c4e493c768d0b8ac36cf8065ea3fd4220c22e9
luka-create <wilosani.luka@gmail.com>
2024-07-12 16:50:04 +0400

    changed folder name

--- src/app/dealer/page.tsx
+++ src/app/dealer/page.tsx
@@ -1,9 +1,39 @@
+'use client'
 import React from 'react'
+import SideBar from './sideBar/SideBar'
+import Header from '@/common/components/header/Header'
+import styled from 'styled-components'
+import UsersListBox from './usersList/UsersListBox'
 
 type Props = {}
 
-const Page = (props: Props) => {
-  return <div>Dealer</div>
+const page = (props: Props) => {
+  return (
+    <Container>
+      <Header />
+      <Frame>
+        <SideBar />
+        <ContentContainer>
+          <UsersListBox />
+        </ContentContainer>
+      </Frame>
+    </Container>
+  )
 }
 
-export default Page
+export default page
+
+const Container = styled.div`
+  background-color: #2020200a;
+`
+const Frame = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  gap: 24px;
+  margin-top: 8px;
+`
+
+const ContentContainer = styled.div`
+  align-items: flex-start;
+`
--- src/app/dealer/usersList/UsersList.tsx
+++ src/app/dealer/usersList/UsersList.tsx
@@ -23,17 +23,17 @@ const recipientListDummy = [
     isVerified: true,
   },
   {
-    fullName: 'Zuka Jakeli',
-    id: '02189491234',
-    mobile: '098 028 11 11',
-    dateOfAddition: '24/04/11',
-    isVerified: true,
+    fullName: 'Elon Musk',
+    id: '06060600000',
+    mobile: '000 06 06 06',
+    dateOfAddition: '00/11/11',
+    isVerified: false,
   },
   {
-    fullName: 'Zuka Jakeli',
-    id: '02189491234',
-    mobile: '098 028 11 11',
-    dateOfAddition: '24/04/11',
+    fullName: 'Siddhartha Gautama',
+    id: '0000000001',
+    mobile: '000 00 00 01',
+    dateOfAddition: '00/00/01',
     isVerified: true,
   },
 ]
@@ -54,25 +54,6 @@ const UserList = (props: Props) => {
       {recipientListDummy.map((data, i) => (
         <ListItem recipientInfo={data} key={`recipient${i}`} />
       ))}
-
-      {/* <ListItem
-        recipientInfo={{
-          fullName: 'Elon Musk',
-          id: '06060600000',
-          mobile: '000 06 06 06',
-          dateOfAddition: '00/11/11',
-          isVerified: false,
-        }}
-      />
-      <ListItem
-        recipientInfo={{
-          fullName: 'Siddhartha Gautama',
-          id: '0000000001',
-          mobile: '000 00 00 01',
-          dateOfAddition: '00/00/01',
-          isVerified: true,
-        }} 
-      />*/}
     </Container>
   )
 }
