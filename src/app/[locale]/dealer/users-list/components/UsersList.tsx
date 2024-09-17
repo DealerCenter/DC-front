04736624ba1c5b00cff18af0905edfd2f8e36cb9
luka-create <wilosani.luka@gmail.com>
2024-09-17 18:21:56 +0400

    UsersList: added pagination logic

--- src/app/[locale]/dealer/users-list/components/UsersList.tsx
+++ src/app/[locale]/dealer/users-list/components/UsersList.tsx
@@ -12,6 +12,8 @@ import Pagination from '@/common/components/pagination/Pagination'
 import UserListEmpty from './UserListEmpty'
 import { message } from 'antd'
 
+const RECEIVERS_PER_PAGE = 10
+
 type Props = { setIsModalOpen: (arg: boolean) => void; searchQuery: string }
 
 const UserList = ({ setIsModalOpen, searchQuery }: Props) => {
@@ -19,25 +21,49 @@ const UserList = ({ setIsModalOpen, searchQuery }: Props) => {
     RECEIVER_GET_RES[] | undefined
   >(undefined)
 
+  const [numOfPages, setNumOfPages] = useState(0)
+  const [receiversToSkip, setReceiversToSkip] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
   const isMobile = useMediaQuery({ query: theme.media?.sm })
   const t = useTranslations('')
-
-  const receiversToSkip = 0
-  const receiversToTake = 16
 
   const getData = async () => {
     const response = await getReceivers({
       skip: receiversToSkip,
-      take: receiversToTake,
+      take: RECEIVERS_PER_PAGE,
       search: searchQuery,
     })
     setReceiversData(response)
   }
 
+  // To get number of pages for the Pagination component
+  const getTotalReceivers = async () => {
+    try {
+      const response = await getReceivers({
+        skip: 0,
+        take: 10000,
+        search: '',
+      })
+      response && setNumOfPages(Math.ceil(response.length / RECEIVERS_PER_PAGE))
+    } catch (error) {
+      console.error('Could not get total receivers')
+    }
+  }
+
+  useEffect(() => {
+    getTotalReceivers()
+    //eslint-disable-next-line
+  }, [])
+
+  useEffect(() => {
+    setReceiversToSkip(RECEIVERS_PER_PAGE * (currentPage - 1))
+  }, [currentPage])
+
+  // getData when search query comes in, or when the page changes
   useEffect(() => {
     getData()
     //eslint-disable-next-line
-  }, [searchQuery])
+  }, [searchQuery, receiversToSkip])
 
   const handleDelete = async (id: number) => {
     try {
@@ -80,9 +106,13 @@ const UserList = ({ setIsModalOpen, searchQuery }: Props) => {
           ))}
         </>
       </Container>
-      {/* <PaginationBox>
-        <Pagination currentPage={1} setCurrentPage={() => {}} numOfPages={2} />
-      </PaginationBox> */}
+      <PaginationBox>
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          numOfPages={numOfPages}
+        />
+      </PaginationBox>
     </>
   )
 }
