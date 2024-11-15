427635e7eb4bfada0227a7dfaba7e063683c1ce4
luka-create <wilosani.luka@gmail.com>
2024-11-15 13:10:57 +0400

    cleanup

--- src/app/[locale]/admin/create-order/components/leftFrame/components/ParametersBox.tsx
+++ src/app/[locale]/admin/create-order/components/leftFrame/components/ParametersBox.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
 import { useTranslations } from 'next-intl'
+import styled from 'styled-components'
 
-import Box from '../../../../components/common/Box'
-import TextInputFieldPair from './TextInputFieldPair'
+import { carCategories } from '@/common/helpers/constants'
 import AddFieldButton from '../../../../components/common/AddFieldButton'
+import Box from '../../../../components/common/Box'
 import {
   FIELD_NAMES,
   useCreateOrderContext,
 } from '../../../hooks/useCreateOrderContext'
-import { carCategories } from '@/common/helpers/constants'
+import TextInputFieldPair from './TextInputFieldPair'
 
 type Props = {}
 
@@ -17,17 +16,6 @@ const ParametersBox = ({}: Props) => {
   const { values, handleBlur, handleChange, errors, touched, setFieldValue } =
     useCreateOrderContext()
   const t = useTranslations('')
-
-  const selectOptions = {
-    option1: {
-      value: 'yes',
-      onChoose: () => setFieldValue(FIELD_NAMES.IS_INSURED, true),
-    },
-    option2: {
-      value: 'no',
-      onChoose: () => setFieldValue(FIELD_NAMES.IS_INSURED, false),
-    },
-  }
 
   const handleChooseIsInsured = (arg: boolean) => {
     setFieldValue(FIELD_NAMES.IS_INSURED, arg)
@@ -45,7 +33,6 @@ const ParametersBox = ({}: Props) => {
         <TextInputFieldPair
           title='insurance'
           value=''
-          // selectOptions={selectOptions}
           onYesOrNoChange={handleChooseIsInsured}
           booleanValue={
             values[FIELD_NAMES.IS_INSURED] && values[FIELD_NAMES.IS_INSURED]
@@ -129,20 +116,6 @@ const ParametersBox = ({}: Props) => {
 
 export default ParametersBox
 
-const IconLabelBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 16px;
-`
-const IconBox = styled.div`
-  width: 24px;
-  height: 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
 const Header = styled.h6`
   margin: 0;
   font-size: ${({ theme }) => theme.fontSizes?.medium};
@@ -162,27 +135,4 @@ const LabelsFrame = styled.div<LabelsFrameProps>`
   display: flex;
   flex-direction: column;
   gap: ${({ isEditing }) => (isEditing ? `8px` : `4px`)};
-`
-
-const LabelPair = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  gap: ${({ theme }) => theme.spacing?.xl};
-  min-height: 36px;
-`
-
-const Label = styled.label`
-  font-size: ${({ theme }) => theme.fontSizes?.small_13};
-  font-weight: ${({ theme }) => theme.fontWeight?.bold};
-  color: ${({ theme }) => theme.colors?.main_gray_68};
-
-  width: 180px;
-`
-
-const Value = styled.label`
-  font-size: ${({ theme }) => theme.fontSizes?.small_13};
-  font-weight: ${({ theme }) => theme.fontWeight?.normal};
-  color: ${({ theme }) => theme.colors?.main_gray_100};
 `
