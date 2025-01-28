ca90eee846b73ae58c4dbdc264f7deafbda76fb1
zuka.j <zuka.jayeli@gmail.com>
2025-01-28 22:49:58 +0530

    Fix: reset password

--- src/app/[locale]/auth/reset-password/ResetPassword.tsx
+++ src/app/[locale]/auth/reset-password/ResetPassword.tsx
@@ -8,46 +8,134 @@ import Image from 'next/image'
 import { useMediaQuery } from 'react-responsive'
 import theme from '../../theme'
 import AppButton from '@/common/components/appButton/AppButton'
+import {
+  resetPassFinalize,
+  resetPassInit,
+  resetPassValidate,
+} from '@/api/apiCalls'
+import { useRouter } from '@/navigation'
+import { endpoints } from '@/api/endpoints'
 
 type Props = {}
 
 const ResetPassword = (props: Props) => {
   const t = useTranslations('')
+  const router = useRouter()
   const isMobile = useMediaQuery({ query: theme.media?.sm })
 
   const [email, setEmail] = useState('')
+  const [otp, setOtp] = useState('')
+  const [password, setPassword] = useState('')
+  const [step, setStep] = useState(0)
+
+  const handleInitReset = async () => {
+    resetPassInit(email)
+    setStep(1)
+  }
+
+  const handleValidate = async () => {
+    resetPassValidate(email, otp)
+    setStep(2)
+  }
+
+  const handleFinalize = async () => {
+    resetPassFinalize(email, password)
+    router.push(endpoints.LOGIN)
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
   return (
     <Container>
       <H4Bold>{t('reset password')}</H4Bold>
-      <TextInputContainer>
-        <TextInput
-          type='email'
-          name='email'
-          placeholder='john@example.com'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          onBlur={() => {}}
-          icon={
-            <Image src={emailIcon} alt='email icon' width={20} height={16} />
-          }
-          width={isMobile ? undefined : 442}
-          // errorMessage={
-          //   errors[FIELD_NAMES.EMAIL] && touched[FIELD_NAMES.EMAIL]
-          //     ? errors[FIELD_NAMES.EMAIL]
-          //     : ''
-          // }
-        />
-      </TextInputContainer>
+      {step === 0 && (
+        <>
+          <TextInputContainer>
+            <TextInput
+              type='email'
+              name='email'
+              placeholder='john@example.com'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => {}}
+              icon={
+                <Image
+                  src={emailIcon}
+                  alt='email icon'
+                  width={20}
+                  height={16}
+                />
+              }
+              width={isMobile ? undefined : 442}
+            />
+          </TextInputContainer>
 
-      <AppButton
-        type='filled'
-        text={t('reset password')}
-        // disabled={isLoading}
-        // onClick={handleSubmit}
-        width={442}
-        htmlType='submit'
-      />
+          <AppButton
+            type='filled'
+            text={t('reset password')}
+            disabled={!emailRegex.test(email)}
+            onClick={handleInitReset}
+            width={442}
+            htmlType='submit'
+          />
+        </>
+      )}
+      {step === 1 && (
+        <>
+          <TextInputContainer>
+            <TextInput
+              type='text'
+              name='OTP'
+              placeholder='Enter one time code'
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+              onBlur={() => {}}
+              icon={
+                <Image
+                  src={emailIcon}
+                  alt='email icon'
+                  width={20}
+                  height={16}
+                />
+              }
+              width={isMobile ? undefined : 442}
+            />
+          </TextInputContainer>
+
+          <AppButton
+            type='filled'
+            text={t('send')}
+            disabled={otp.length === 0}
+            onClick={handleValidate}
+            width={442}
+            htmlType='submit'
+          />
+        </>
+      )}
+      {step === 2 && (
+        <>
+          <TextInputContainer>
+            <TextInput
+              type='password'
+              name='password'
+              placeholder='Enter new password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => {}}
+              width={isMobile ? undefined : 442}
+            />
+          </TextInputContainer>
+
+          <AppButton
+            type='filled'
+            text={t('reset password')}
+            disabled={password.length === 0}
+            onClick={handleFinalize}
+            width={442}
+            htmlType='submit'
+          />
+        </>
+      )}
     </Container>
   )
 }
