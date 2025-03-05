0faa83bafeaa5ca799a1e8919467a5a4e29ae61e
zuka.j <zuka.jayeli@gmail.com>
2025-03-05 18:00:45 +0400

    Fix: build error

--- src/app/[locale]/search-vehicle/inbox-car/[id]/layout.tsx
+++ src/app/[locale]/search-vehicle/inbox-car/[id]/layout.tsx
@@ -1,8 +1,6 @@
 'use client'
 import Header from '@/common/components/header/Header'
-import FullHeader from '@/common/components/header/components/fullHeader/FullHeader'
 import { Inter } from 'next/font/google'
-import styled from 'styled-components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,18 +11,18 @@ export default function RootLayout({
 }>) {
   return (
     <section className='vehicle-listing-layout'>
-      <StickyHeader>
+      <div
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1000,
+          backgroundColor: '#fff',
+          boxShadow: '0 1px 2px -2px black',
+        }}
+      >
         <Header />
-      </StickyHeader>
+      </div>
       {children}
     </section>
   )
 }
-
-export const StickyHeader = styled.div`
-  position: sticky;
-  top: 0;
-  z-index: 1000;
-  background-color: #fff;
-  box-shadow: 0 1px 2px -2px black;
-`
